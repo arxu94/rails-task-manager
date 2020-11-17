83b5752a76f2a1d08c2842f9364db28b8d79e63c
Ariel Xu <arielxu@live.com>
2020-11-17 14:50:37 +0800

    added an index to view all tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+  # line break
+  def index
+    @tasks = Task.all
+    # @completed = task.completed
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # read all tasks
+# read all tasks
+get '/tasks', to: 'tasks#index'
 
-  # read one singular task
-  # create new task
-  # edit one singular task
-  # delete a task
+# create new task
+get '/tasks/new', to: 'tasks#new'
+post '/tasks', to: 'tasks#create'
+
+# read one singular task
+get '/tasks/:id', to: 'tasks#show'
+
+# edit one singular task
+get '/tasks/:id/edit', to: 'tasks#edit'
+patch '/tasks', to: 'tasks#update'
+
+# delete a task
+delete '/tasks/:id', to: 'tasks#destroy'
 end
